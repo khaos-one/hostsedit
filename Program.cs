bb38680950c5956b09c13c5ab99ee1e231a1fe91
Егор Зеленский <i@khaos.su>
2016-09-24 16:00:00 +0300

    Initial project commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HostsEdit
-{
-    static class Program
-    {
+namespace HostsEdit {
+    static class Program {
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var isAdmin = false;
+
+            try {
+                var user = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException) { }
+
+            if (!isAdmin) {
+                MessageBox.Show("The program requires to be run with administrator privileges.", "Run as administrator",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            else {
+                Application.Run(new MainForm());
+            }
         }
     }
 }
